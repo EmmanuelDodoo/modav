dda8e6890348f4604b03c678eb579e99a4ec6f0c
EmmanuelDodoo <custosdodoo@gmail.com>
2024-01-19 14:01:09 -0500

    Added status bar

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,101 @@
 use iced::{
-    executor,
-    theme::{self},
-    widget::{self, column, container, horizontal_space, row, text, vertical_space, Column, Row},
-    Application, Command, Element, Length, Renderer, Settings, Theme,
+    color, executor, font, theme,
+    widget::{
+        column, container, horizontal_space, row, text, vertical_rule, vertical_space, Container,
+        Row,
+    },
+    Application, Command, Length, Settings, Theme,
 };
-use std::{fmt, path::PathBuf};
+use std::path::PathBuf;
 
-use menu::{create_menu, pick_file};
-use styles::BorderedContainer;
+mod temp;
+use styles::*;
+// use temp::TApp;
+use utils::*;
 
 fn main() -> Result<(), iced::Error> {
-    TApp::run(Settings::default())
+    Modav::run(Settings::default())
+    // TApp::run(Settings::default())
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub enum TError {
-    DialogClosed,
-}
-
-impl fmt::Display for TError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::DialogClosed => write!(f, "Dialog closed prematurely"),
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Default)]
-pub enum TMessage {
-    #[default]
-    None,
-    ToggleTheme,
-    Open,
-    FileOpened(Result<PathBuf, TError>),
-    Quit,
-}
-
-#[derive(Debug, Clone)]
-struct TApp {
-    title: String,
-    recent_error: Option<TError>,
-    current_file_path: PathBuf,
+pub struct Modav {
     theme: Theme,
-    is_dark: bool,
+    title: String,
+    current_model: String,
+    file_path: PathBuf,
+    error: AppError,
 }
 
-impl TApp {
-    fn create_shell<'a>(
-        &self,
-        content: Element<'a, TMessage, Renderer>,
-    ) -> widget::Container<'a, TMessage, Renderer> {
-        let rw = row!(
-            horizontal_space(Length::Fixed(5.0)),
-            create_menu(),
-            horizontal_space(Length::Fill)
-        );
-        let menu = container(rw).style(theme::Container::Custom(Box::new(BorderedContainer {})));
+#[derive(Debug, Clone, PartialEq)]
+pub enum Message {
+    FontLoaded(Result<(), font::Error>),
+}
 
-        let status = {
-            let err_text = {
-                let color = iced::Color {
-                    r: 1.0,
-                    g: 0.0,
-                    b: 0.2,
-                    a: 1.0,
+impl Modav {
+    fn status_bar(&self) -> Container<'_, Message> {
+        let current: Row<'_, Message> = {
+            let model = if self.current_model.is_empty() {
+                None
+            } else {
+                Some(text(self.current_model.clone()))
+            };
+
+            let path = {
+                let path = match self.file_path.file_name() {
+                    Some(s) => s.to_str().unwrap_or(""),
+                    None => "",
                 };
-                match self.recent_error.clone() {
-                    Some(err) => text(format!("{}", err)).style(theme::Text::Color(color)),
-                    None => text(""),
+                if path.is_empty() {
+                    None
+                } else {
+                    let t = text(path);
+                    let icon = status_icon('\u{F0F6}');
+                    Some(row!(icon, t).spacing(5))
                 }
             };
-            let path = match self.current_file_path.to_str() {
-                Some(p) => text(p),
-                None => text(""),
-            };
-            let row: Row<'_, TMessage> =
-                row!(err_text, horizontal_space(Length::Fill), path).padding([5, 10]);
 
-            container(row).style(theme::Container::Custom(Box::new(BorderedContainer {})))
+            match (path, model) {
+                (None, None) => row!(),
+                (None, Some(m)) => row!(m),
+                (Some(p), None) => row!(p),
+                (Some(p), Some(m)) => row!(m, vertical_rule(2), p),
+            }
+        }
+        .spacing(10);
+
+        let error = {
+            let color = color!(255, 0, 0);
+            let msg = self.error.message();
+            if msg.is_empty() {
+                row!()
+            } else {
+                let icon = text("•").style(theme::Text::Color(color));
+                row!(icon, text(msg)).spacing(5)
+            }
         };
 
-        let col: Column<'_, TMessage, Renderer> = column!(
-            menu,
-            vertical_space(Length::Fill),
-            content,
-            vertical_space(Length::Fill),
-            status
-        );
+        let row: Row<'_, Message> = row!(error, horizontal_space(Length::Fill), current).height(20);
 
-        container(col)
+        let bstyle = match self.theme {
+            Theme::Dark => BorderedContainer {
+                bcolor: color!(0xffffff),
+                ..Default::default()
+            },
+            Theme::Light => BorderedContainer::default(),
+            Theme::Custom(_) => BorderedContainer::default(),
+        };
+
+        container(row)
+            .padding([3, 10])
+            .style(theme::Container::Custom(Box::new(bstyle)))
     }
 }
 
-impl Application for TApp {
+impl Application for Modav {
     type Theme = Theme;
     type Flags = ();
-    type Message = TMessage;
+    type Message = Message;
     type Executor = executor::Default;
 
     fn title(&self) -> String {
@@ -108,201 +108,108 @@ impl Application for TApp {
 
     fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
         (
-            TApp {
-                current_file_path: PathBuf::new(),
-                title: String::from("Playground"),
-                recent_error: None,
-                theme: Theme::Light,
-                is_dark: false,
+            Modav {
+                file_path: PathBuf::new(),
+                title: String::from("Modav"),
+                theme: Theme::Dark,
+                current_model: String::new(),
+                error: AppError::None,
             },
-            Command::none(),
+            font::load(include_bytes!("../fonts/status-icons.ttf").as_slice())
+                .map(Message::FontLoaded),
         )
     }
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
-            TMessage::ToggleTheme => {
-                self.is_dark = !self.is_dark;
-                self.theme = if self.is_dark {
-                    Theme::Dark
-                } else {
-                    Theme::Light
-                };
-                self.recent_error = None;
+            Message::FontLoaded(Ok(_)) => Command::none(),
+            Message::FontLoaded(Err(e)) => {
+                self.error = AppError::Font(e);
                 Command::none()
-            }
-            TMessage::None => {
-                self.recent_error = None;
-                Command::none()
-            }
-            TMessage::Open => {
-                self.recent_error = None;
-                Command::perform(pick_file(), TMessage::FileOpened)
-            }
-            TMessage::FileOpened(Ok(p)) => {
-                self.current_file_path = p;
-                self.recent_error = None;
-                Command::none()
-            }
-            TMessage::FileOpened(Err(err)) => {
-                self.recent_error = Some(err);
-                Command::none()
-            }
-            TMessage::Quit => {
-                std::process::exit(0);
             }
         }
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
-        let content = row!();
-        let shell = self.create_shell(content.into());
-        container(shell).into()
+        let status_bar = self.status_bar();
+        let text = text("Some text");
+
+        let main_axis = column!(text, vertical_space(Length::Fill), status_bar);
+
+        container(main_axis).into()
     }
 }
 
-mod menu {
-    use super::{
-        styles::MenuButtonStyle,
-        {TError, TMessage},
-    };
-    use iced::widget::{button, text};
-    use iced::{theme, Element, Length, Renderer};
-    use iced_aw::{menu_tree, MenuBar};
-    use iced_aw::{
-        native::helpers::{menu_bar, menu_tree},
-        MenuTree,
-    };
-    use std::path::PathBuf;
+mod utils {
+    use std::fmt::{Debug, Display};
 
-    pub async fn pick_file() -> Result<PathBuf, TError> {
-        let handle = rfd::AsyncFileDialog::new()
-            .pick_file()
-            .await
-            .ok_or(TError::DialogClosed)?;
-
-        Ok(handle.path().into())
-    }
-
-    pub fn create_menu<'a>() -> MenuBar<'a, TMessage, Renderer> {
-        let menus: Vec<MenuTree<'_, TMessage, Renderer>> =
-            vec![create_file_menu(), create_view_menu(), create_help_menu()];
-        menu_bar(menus)
-            .spacing(15.0)
-            .padding([5, 0])
-            .main_offset(15)
-            .bounds_expand(30)
-    }
-
-    fn create_view_menu<'a>() -> MenuTree<'a, TMessage, Renderer> {
-        let labels = vec![
-            ("Toggle Theme", TMessage::ToggleTheme),
-            ("Theme", TMessage::None),
-        ];
-        let children = create_children(labels);
-        let name = create_name("View", None);
-        menu_tree(name, children)
-    }
-
-    fn create_help_menu<'a>() -> MenuTree<'a, TMessage, Renderer> {
-        let name = create_name("Help", None);
-
-        menu_tree!(name)
-    }
-
-    fn base_tree<'a>(label: &'a str, msg: TMessage) -> MenuTree<'a, TMessage, Renderer> {
-        let btn = button(text(label).width(Length::Fill).height(Length::Fill))
-            .on_press(msg)
-            .style(theme::Button::Custom(Box::new(MenuButtonStyle {})))
-            .padding([4, 8])
-            .width(Length::Fill)
-            .height(Length::Shrink);
-
-        menu_tree!(btn)
-    }
-
-    fn create_children<'a>(
-        labels: Vec<(&'a str, TMessage)>,
-    ) -> Vec<MenuTree<'a, TMessage, Renderer>> {
-        labels
-            .into_iter()
-            .map(|curr| {
-                let label = curr.0;
-                let msg = curr.1;
-                base_tree(label, msg)
-            })
-            .collect()
-    }
-
-    fn create_name<'a>(
-        name: &'a str,
-        msg: Option<TMessage>,
-    ) -> impl Into<Element<'a, TMessage, Renderer>> {
-        let content = text(name).width(Length::Fill).height(Length::Fill);
-
-        let btn = match msg {
-            Some(msg) => button(content).on_press(msg),
-            None => button(content),
-        }
-        .style(theme::Button::Custom(Box::new(MenuButtonStyle {})))
-        .padding([4, 8]);
-
-        btn
-    }
-
-    fn create_file_menu<'a>() -> MenuTree<'a, TMessage, Renderer> {
-        let labels = vec![("Open File", TMessage::Open), ("Exit", TMessage::Quit)];
-        let children = create_children(labels);
-        let name = create_name("File", None);
-        menu_tree(name, children)
-    }
-}
-
-mod styles {
     use iced::{
-        widget::{button, container},
-        Color, Theme,
+        alignment, font,
+        widget::{text, Text},
+        Font,
     };
 
-    pub struct MenuButtonStyle;
-    impl button::StyleSheet for MenuButtonStyle {
-        type Style = iced::Theme;
+    #[derive(Debug, Clone, PartialEq, Default)]
+    pub enum AppError {
+        Font(font::Error),
+        #[default]
+        None,
+    }
 
-        fn active(&self, style: &Self::Style) -> button::Appearance {
-            button::Appearance {
-                text_color: style.extended_palette().background.base.text,
-                border_radius: [4.0; 4].into(),
-                background: Some(Color::TRANSPARENT.into()),
-                ..Default::default()
-            }
-        }
-
-        fn hovered(&self, style: &Self::Style) -> button::Appearance {
-            let plt = style.extended_palette();
-
-            button::Appearance {
-                background: Some(plt.primary.weak.color.into()),
-                text_color: plt.primary.weak.text,
-                ..self.active(style)
+    impl AppError {
+        pub fn message(&self) -> String {
+            match self {
+                Self::Font(_) => String::from("Error while loading a font"),
+                Self::None => String::new(),
             }
         }
     }
 
-    pub struct BorderedContainer;
+    impl Display for AppError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                Self::Font(e) => e.fmt(f),
+                Self::None => write!(f, ""),
+            }
+        }
+    }
+
+    pub fn icon(unicode: char, name: &'static str) -> Text<'static> {
+        let fnt: Font = Font::with_name(name);
+        text(unicode.to_string())
+            .font(fnt)
+            .horizontal_alignment(alignment::Horizontal::Center)
+    }
+
+    pub fn status_icon(unicode: char) -> Text<'static> {
+        icon(unicode, "status-icons")
+    }
+}
+
+pub mod styles {
+    use iced::{color, widget::container, Color, Theme};
+
+    pub struct BorderedContainer {
+        pub width: f32,
+        pub bcolor: Color,
+    }
+
+    impl Default for BorderedContainer {
+        fn default() -> Self {
+            Self {
+                width: 0.5,
+                bcolor: color!(0, 0, 0, 1.),
+            }
+        }
+    }
+
     impl container::StyleSheet for BorderedContainer {
         type Style = Theme;
 
         fn appearance(&self, _style: &Self::Style) -> container::Appearance {
-            let color = Color {
-                r: 0.0,
-                g: 0.0,
-                b: 0.0,
-                a: 1.0,
-            };
-
             container::Appearance {
-                border_width: 0.5,
-                border_color: color,
+                border_width: self.width,
+                border_color: self.bcolor,
                 ..Default::default()
             }
         }
